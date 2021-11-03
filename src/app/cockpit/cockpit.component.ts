077442832e192195c1240eba8680f5d53a9e7c09
Destoky <adestoky@gmail.com>
2021-11-03 17:54:28 +0100

    70. Assigning an Alias to Custom Events

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
-  @Output() serverCreated = new EventEmitter<{
+  @Output("srvCreated") serverCreated = new EventEmitter<{
     serverName: string;
     serverContent: string;
   }>();
