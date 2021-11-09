c829344e02d2d5450531a466bb8728b32699871f
Destoky <adestoky@gmail.com>
2021-11-09 11:53:06 +0100

    Calcul TVA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  montant: number;
+  tva: number;
+  total: number;
 
+  getValue(event: Event): number {
+    return +(event.target as HTMLInputElement).value;
+  }
+
+  calcTotal() {
+    if (!this.tva || !this.montant) return;
+    this.total = +(this.montant * (1 + this.tva / 100)).toFixed(2);
+  }
+
+  calcHTVA() {
+    if (!this.tva) return;
+    this.montant = +(this.total * (100 / (100 + this.tva))).toFixed(2);
+  }
 }
