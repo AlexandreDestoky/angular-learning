e19f5d527c74fd0e9626fbed3f6a6e232f39370b
Destoky <adestoky@gmail.com>
2021-11-10 22:22:53 +0100

    Highlight Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
