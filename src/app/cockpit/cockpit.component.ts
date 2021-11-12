392f2aa91c19ffb68aa5efc11e1d1e5c607263fb
Destoky <adestoky@gmail.com>
2021-11-12 14:50:09 +0100

    ngcontent

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -6,8 +6,7 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
   styleUrls: ['./cockpit.component.css'],
 })
 export class CockpitComponent implements OnInit {
-  // newServerName = '';
-  // newServerContent = '';
+
   @Output() addServer = new EventEmitter<{
     serverName: string;
     serverContent: string;
@@ -20,28 +19,12 @@ export class CockpitComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
-  // ServerEvent() {
-  //   this.addServer.emit({
-  //     serverName: this.newServerName,
-  //     serverContent: this.newServerContent,
-  //   });
-  //   this.clearInput();
-  // }
-  // BlueprintEvent() {
-  //   this.addBluePrint.emit({
-  //     serverName: this.newServerName,
-  //     serverContent: this.newServerContent,
-  //   });
-  //   this.clearInput();
+
   ServerEvent(name,content) {
         this.addServer.emit({
       serverName: name,
       serverContent: content,
     });
   }
-  
-  clearInput(input1,input2) {
-    input1.value = "";
-    input2.value = "";
-  }
+
 }
