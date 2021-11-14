a238137299c219a5d3dcea707a16ade0d23014f2
Destoky <adestoky@gmail.com>
2021-11-14 20:41:12 +0100

    Creation dossier pour Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HighlightDirective } from './highlight.directive';
-import { AbbrevPipe } from './abbrev.pipe';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { ServerElementComponent } from './server-element/server-element.component';
+import { AppRoutingModule } from './app-routing.module';
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HighlightDirective,
-    AbbrevPipe,
-    CockpitComponent,
-    ServerElementComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/card.modele.ts
+++
@@ -1,9 +0,0 @@
-export class Card {
-  constructor(
-    public imgUrl: string,
-    public prenom: string,
-    public nom: string,
-    public note: number,
-    public txt: string
-  ) {}
-}
