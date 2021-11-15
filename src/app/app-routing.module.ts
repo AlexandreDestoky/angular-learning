7e367277c81105ba32e5209836348034253147df
Destoky <adestoky@gmail.com>
2021-11-15 17:30:59 +0100

    133. Passing Parameters to Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
+import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'users', component: UserComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'users/:id', component: UserComponent },
   { path: 'home', component: HomeComponent },
   { path: 'servers', component: ServersComponent },
   { path: '**', component: ErrorComponent },
