27ab41535b69694e9547dbccfcbc88a780a8e105
Destoky <adestoky@gmail.com>
2021-11-15 17:18:08 +0100

    132. Using Relative Paths in Programming Navigation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,19 +4,13 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor(private router:Router) { }
-
-  ngOnInit() {
-    setTimeout(()=> {
-      this.onLoadServers()
-    },2000);
-  }
+  ngOnInit() {}
   onLoadServers() {
     this.router.navigate(['/servers']);
   }
-
 }
