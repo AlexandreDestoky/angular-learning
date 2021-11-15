2b1f3793da82f35e9d134a333f7e319625dab1df
Destoky <adestoky@gmail.com>
2021-11-15 13:06:32 +0100

    127. Setting up and Loading Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router'; // CLI imports router
+import { HomeComponent } from './home/home.component';
+import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'users', component: UserComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'servers', component: ServersComponent },
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,9 @@
 import { Component } from '@angular/core';
-import { LoggerService } from './logger.service';
-import { Product } from './product.modele';
-import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ProductService, LoggerService],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  products: Product[];
-
-  constructor(private productService: ProductService) {}
-  getProducts() {
-    this.products = this.productService.getProducts();
-  }
 }
