8a3cd866dcec66f1c5c2b7c7fe1d1dd156030c1b
Destoky <adestoky@gmail.com>
2021-11-16 16:30:55 +0100

    137. Passing Query Parameters and Fragments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { ErrorComponent } from './error/error.component';
 import { HomeComponent } from './home/home.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { UsersComponent } from './users/users.component';
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: 'users/:id/:name', component: UserComponent },
   { path: 'home', component: HomeComponent },
   { path: 'servers', component: ServersComponent },
+  { path: 'servers/:id/edit', component: EditServerComponent },
   { path: '**', component: ErrorComponent },
 ];
 
