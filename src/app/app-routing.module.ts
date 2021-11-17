fa8c43a6e3712f196145fc521c803afa18142772
Destoky <adestoky@gmail.com>
2021-11-17 21:59:47 +0100

    151. Passing a Static Data to a Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
     component: ServersComponent,
     children: [
       { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent },
+      { path: ':id/edit', component: EditServerComponent , data:{message:"Page Test"}},
     ],
   },
   { path: '**', component: ErrorComponent },
