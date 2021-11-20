9b82adf22d6faa3181ce2dca257c6d876f8ff2ba
Destoky <adestoky@gmail.com>
2021-11-20 16:53:09 +0100

    Premiere utilisation de get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,43 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Repos } from './repos.modele';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  userName: string = 'AlexandreDestoky';
+  baseURL: string = 'https://api.github.com/';
+  repos: Repos[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getRepos();
+  }
+
+  public getRepos() {
+    return this.http
+      .get<Repos[]>(this.baseURL + 'users/' + this.userName + '/repos')
+      .subscribe(
+        (response) => {
+          //Next callback
+          console.log('response received');
+          console.log(response);
+          this.repos = response;
+        },
+        (error) => {
+          //Error callback
+          console.error('Request failed with error');
+          alert(error);
+        },
+        () => {
+          //Complete callback
+          console.log('Request completed');
+        }
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,22 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { UsersComponent } from './users/users.component';
-import { ServersComponent } from './servers/servers.component';
-import { UserComponent } from './users/user/user.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { AppRoutingModule } from './app-routing.module';
-import { ErrorComponent } from './error/error.component';
+import { HttpClientModule } from '@angular/common/http';
+
+// import { AppRoutingModule } from './app-routing.module';
+
 
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    UsersComponent,
-    ServersComponent,
-    UserComponent,
-    EditServerComponent,
-    ServerComponent,
-    ErrorComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
