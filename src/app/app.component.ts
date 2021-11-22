ab7bb4b093050458c4f4593afd1b3f21451bb4cd
Destoky <adestoky@gmail.com>
2021-11-22 09:52:48 +0100

    257. GETting Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,24 +10,39 @@ export class AppComponent implements OnInit {
   loadedPosts = [];
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
-    this.http.post(
-      'https://test-angular-f9eba-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
-      postData
-    ).subscribe(responseData => {
-      console.log(responseData);
-    })
+    this.http
+      .post(
+        'https://test-angular-f9eba-default-rtdb.europe-west1.firebasedatabase.app/posts.json',
+        postData
+      )
+      .subscribe((responseData) => {
+        console.log(responseData);
+      });
   }
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://test-angular-f9eba-default-rtdb.europe-west1.firebasedatabase.app/posts.json'
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
