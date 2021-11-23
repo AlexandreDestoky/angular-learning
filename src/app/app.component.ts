ddd05f4677c8ba35ea9cbd9550b0e6a6c6695a5b
Destoky <adestoky@gmail.com>
2021-11-23 09:47:58 +0100

    265. Handling Errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,20 +12,27 @@ import { PostsService } from './posts.service';
 export class AppComponent implements OnInit {
   loadedPosts = [];
   isFetching = false;
+  error = null;
 
   constructor(private http: HttpClient, private postsService: PostsService) {}
 
   ngOnInit() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-
-    })
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        // console.log(posts);
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.error = error.message;
+        console.log(error.error)
+      }
+    );
   }
 
   onCreatePost(postData: Post) {
-    this.postsService.createAndStorePost(postData.title,postData.content);
+    this.postsService.createAndStorePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
@@ -35,18 +42,22 @@ export class AppComponent implements OnInit {
 
   onClearPosts() {
     // Send Http request
-    this.postsService.deletePosts().subscribe(()=> {
+    this.postsService.deletePosts().subscribe(() => {
       this.loadedPosts = [];
-    })
+    });
   }
 
   private fetchPosts() {
     this.isFetching = true;
-    this.postsService.fetchPosts().subscribe((posts) => {
-      // console.log(posts);
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    });
-
+    this.postsService.fetchPosts().subscribe(
+      (posts) => {
+        // console.log(posts);
+        this.isFetching = false;
+        this.loadedPosts = posts;
+      },
+      (error) => {
+        this.error = error.message;
+      }
+    );
   }
 }
