2b7e76231081bd944e315e74f12cf9dd5796cfdd
Destoky <adestoky@gmail.com>
2021-11-28 18:02:16 +0100

    Set Value in Template Driven forms in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Form, NgForm } from '@angular/forms';
+import { contactForm } from './contactForm.modele';
 import { country } from './country.modele';
 
 @Component({
@@ -8,9 +10,19 @@ import { country } from './country.modele';
 })
 export class AppComponent implements OnInit {
   constructor() {}
+  contact: contactForm;
 
-  ngOnInit() {}
-  title = 'Template driven forms';
+  ngOnInit() {
+    this.contact = {
+      firstname: 'Sachin',
+      lastname: 'Tendulkar',
+      email: 'sachin@gmail.com',
+      gender: 'male',
+      isMarried: true,
+      country: '2',
+      address: { city: 'Mumbai', street: 'Perry Cross Rd', pincode: '400050' },
+    };
+  }
 
   countryList: country[] = [
     new country('1', 'India'),
@@ -18,11 +30,9 @@ export class AppComponent implements OnInit {
     new country('3', 'England'),
   ];
 
-  affichage(form) {
+  onSubmit(form : NgForm) {
     console.log(form.value);
-    console.log("----");
+    console.log('----');
+    form.resetForm();
   }
-
 }
-
-
