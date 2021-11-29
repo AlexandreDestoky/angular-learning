a9836748b72b0fb7a5e4967af55758d8c4ba783a
Destoky <adestoky@gmail.com>
2021-11-29 10:54:30 +0100

    205. Reactive: Syncing HTML and Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   
   ngOnInit() {
     this.signupForm= new FormGroup({
-      username: new FormControl(null),
+      username: new FormControl("Alex"),
       email:new FormControl(null),
       gender:new FormControl("male")
     })
