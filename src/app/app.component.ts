dcb5ac6e0e491e5de924d430b8407afa68d6c638
Destoky <adestoky@gmail.com>
2021-11-29 10:35:40 +0100

    203. Reactive: Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,38 +7,9 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  @ViewChild('formulaire') valeurForm: NgForm;
-  genders = ['male', 'femme'];
-  defaultQuestion = 'teacher';
-  user = {
-    username: '',
-    email: '',
-    secretQuestion: '',
-    answer: '',
-    gender: '',
-  };
-  submitted= false;
-
   ngOnInit() {}
+  genders = ['male', 'female'];
 
-  suggestUserName() {
-    const suggestedName = 'Superuser';
-    this.valeurForm.form.patchValue({
-      userData: {
-        username: suggestedName,
-      },
-    });
-  }
-
-  affiche() {
-    console.log(this.valeurForm.value);
-    this.submitted = true;
-    this.user.username = this.valeurForm.value.userData.username;
-    this.user.email = this.valeurForm.value.userData.email;
-    this.user.secretQuestion = this.valeurForm.value.question;
-    this.user.answer = this.valeurForm.value.questionAnswer;
-    this.user.gender = this.valeurForm.value.gender;
-
-    this.valeurForm.reset();
-  }
+  signupForm : FormGroup
+  
 }
